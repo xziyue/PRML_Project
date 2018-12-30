119d31396eb6fa2ca65291957170310fef274361
Alan Xiang <xziyue@syr.edu>
2018-12-29 22:21:25 -0500

    20181230update

--- compile_all.py
+++ compile_all.py
@@ -1,11 +1,20 @@
 # compile all tex answers
 from script.util import *
+import operator
 
 root = os.getcwd()
 toc = GetAnswerTOC(root)
 
-allAns = [val for _, val in toc['tex'].items()]
-allAns.sort()
+def GetTuple(name):
+    fname = os.path.basename(name)
+    name, _ = os.path.splitext(fname)
+    nums = name.split('.')
+    return tuple(map(int, nums))
+
+
+_allAns = [(GetTuple(val), val) for _, val in toc['tex'].items()]
+_allAns.sort(key=operator.itemgetter(0))
+allAns = [val[1] for val in _allAns]
 
 allFrac = ''
 for path in allAns:
